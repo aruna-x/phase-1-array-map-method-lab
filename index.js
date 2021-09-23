812e77c41c4d79c8451ff7f89a903dfd02be48dd
Aruna <aruna.evan15@gmail.com>
2021-09-22 21:43:38 -0700

    commit tree 2e95d1d2a0ecb85bf7021955a58f5d5c7087a265

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map((title) => {
+    let wordsArray = title.split(" ");
+    const res = wordsArray.map((word) => {
+      return word[0].toUpperCase() + word.slice(1);
+    });
+    return res.join(" ");
+  });
 }
